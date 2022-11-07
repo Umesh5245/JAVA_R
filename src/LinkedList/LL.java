9db6922adfba1fa5f34a7ad53e3e8a96d6a9ee21
Umesh5245 <umeshagarwal5245@gmail.com>
2022-11-08 05:17:54 +0530

    Linked list

--- src/LinkedList/LL.java
+++ src/LinkedList/LL.java
@@ -3,7 +3,7 @@ package LinkedList;
 public class LL {
     private Node head;
     private Node tail;
-    private int size = 0;
+    private int size;
 
     public LL() {
         this.size = 0;
@@ -13,9 +13,66 @@ public class LL {
         Node node = new Node(val);
         node.next = head;
         head = node;
-        if (tail == null)
-            tail = head;
+        if (tail == null) tail = head;
         size++;
+    }
+
+    public void insertMiddle(int val, int index) {
+        if (index == 0) insertFirst(val);
+        if (index == size) insertLast(val);
+        int i = 1;
+        Node temp = head;
+        while (i < index) {
+            temp = temp.next;
+            if (index - 1 == i) {
+                temp.next = new Node(val, temp.next);
+                break;
+            }
+            i++;
+        }
+        size++;
+    }
+
+    public int delete(int index) {
+        if (index == 0) delteFirst();
+        if (index == size - 1) deleteLast();
+        Node prevnode = get(index - 1);
+        Node nextnode = get(index + 1);
+        int val = prevnode.next.value;
+        prevnode.next = nextnode;
+        size--;
+        return val;
+    }
+
+    public int deleteLast() {
+        if (size <= 1) {
+            return delteFirst();
+        }
+
+        Node secondLast = get(size - 2);
+        int val = tail.value;
+        tail = secondLast;
+        tail.next = null;
+        size--;
+        return val;
+    }
+
+    public Node get(int index) {
+        Node node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    public int delteFirst() {
+        int val = head.value;
+        head = head.next;
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+        return val;
     }
 
     public void insertLast(int val) {
@@ -35,7 +92,7 @@ public class LL {
         System.out.println("SEE YOU TATA BYE BYE");
     }
 
-    private class Node {
+    private static class Node {
 
         private final int value;
         private Node next;
--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -8,10 +8,14 @@ public class Main {
         list.insertFirst(2);
         list.insertFirst(3);
         list.insertFirst(4);
-        list.insertFirst(4);
+        list.insertFirst(5);
         list.insertLast(66);
         list.insertLast(77);
-
+        list.insertMiddle(44, 3);
+        list.display();
+          System.out.println("Deleted "+list.delteFirst());
+        System.out.println("Deleted "+list.deleteLast());
+        System.out.println("Deleted " + list.delete(4));
         list.display();
     }
 }
